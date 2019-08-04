3090ba9fd741bae503ee3a9798d442149d3f0c30
Brian Cherin <brian.cherin@gmail.com>
2019-08-04 12:52:46 -0400

    Refactor book list page to make code more aligned with SRP

--- src/components/BookList.component.js
+++ src/components/BookList.component.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { fetchSavedBooks } from '../api/audioStorage';
 
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import { ListItemText, Avatar, Menu, MenuItem } from '@material-ui/core';
+import { ListItemText, Avatar, Menu, MenuItem, Fab } from '@material-ui/core';
+
+
 
 class BookList extends React.Component {
 
     state = {
         fileKeys: [],
         menuAnchorElement: null
-        
     }
 
     async componentDidMount() {
@@ -47,37 +48,64 @@ class BookList extends React.Component {
                     return(
                         <Card key={key}>
                             <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar alt={key}  src="" style={{borderRadius:0}}/>
-                                </ListItemAvatar>
+                                {this.renderBookImage()}
                                 <ListItemText>{key}</ListItemText>
-                                <ListItemSecondaryAction>
-                                <IconButton aria-controls={`menu-$(key)`} aria-haspopup="true" onClick={this.handleMenuClick} edge="end" style={{marginLeft:'auto'}}>
-                                    <MoreVertIcon />
-                                </IconButton>
-                                </ListItemSecondaryAction>
+                                {this.renderBookMenu()}
                             </ListItem>
-
-                            <Menu 
-                                id={`menu-$(key)`} 
-                                anchorEl={this.state.menuAnchorElement} 
-                                onClose={this.handleMenuClose}
-                                open={Boolean(this.state.menuAnchorElement)}
-                            >
-                                <MenuItem>Remove from library</MenuItem>
-                            </Menu>
-
                         </Card>
                     );
                 })}
             </List>
         );
-    }         
+    }
+    
+    renderBookImage(bookKey) {
+        return(
+            <ListItemAvatar>
+                <Avatar alt={bookKey}  src="" style={{borderRadius:0}}/>
+            </ListItemAvatar>
+        );
+    }
+
+    renderBookMenu(bookKey) {
+        return(
+            <ListItemSecondaryAction>
+                <IconButton 
+                    aria-controls={`menu-$(bookKey)`} 
+                    aria-haspopup="true" onClick={this.handleMenuClick} 
+                    edge="end" style={{marginLeft:'auto'}}
+                >
+                    <MoreVertIcon />
+                </IconButton>
+                <Menu 
+                    id={`menu-$(key)`} 
+                    anchorEl={this.state.menuAnchorElement} 
+                    onClose={this.handleMenuClose}
+                    open={Boolean(this.state.menuAnchorElement)}
+                >
+                    <MenuItem>Remove from library</MenuItem>
+                </Menu>
+            </ListItemSecondaryAction>
+        );
+    }
+
+    renderUploadButton() {
+        return(
+            <Fab 
+                variant="extended" 
+                aria-label="upload"
+                style={{position:'absolute', bottom:'20%', right:'20%'}}
+            >
+                Upload an audiobook
+            </Fab>
+        );
+    }
 
     render() {
         return(
             <div>
                 {this.renderSavedBooks()}
+                {this.renderUploadButton()}                
             </div>
         );
     }
